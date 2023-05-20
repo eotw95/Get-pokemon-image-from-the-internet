033b4baed43ab821db12450991629063578136ba
Ryuji Koyama <ryujikoyama95@gmail.com>
2023-05-20 23:26:19 +0900

    Add PokeListApp

--- app/src/main/java/com/example/pokelist/ui/theme/PokeListApp.kt
+++ app/src/main/java/com/example/pokelist/ui/theme/PokeListApp.kt
@@ -1,2 +1,12 @@
 package com.example.pokelist.ui.theme
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.pokelist.ui.theme.screens.HomeScreen
+import com.example.pokelist.ui.theme.screens.PokeViewModel
+
+@Composable
+fun PokeListApp(modifier: Modifier = Modifier) {
+    val pokeViewModel = PokeViewModel()
+    HomeScreen(pokeUiState = pokeViewModel.pokeUiState)
+}
