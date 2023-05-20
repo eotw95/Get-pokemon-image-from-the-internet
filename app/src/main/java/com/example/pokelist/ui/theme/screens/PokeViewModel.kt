2db30e6e3f0e07f9a13e742bc0bf07c37dbc6faa
Ryuji Koyama <ryujikoyama95@gmail.com>
2023-05-20 22:06:28 +0900

    Add viewmodel

--- app/src/main/java/com/example/pokelist/ui/theme/screens/PokeViewModel.kt
+++ app/src/main/java/com/example/pokelist/ui/theme/screens/PokeViewModel.kt
@@ -1,14 +1,31 @@
 package com.example.pokelist.ui.theme.screens
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.pokelist.network.PokeApi
 import kotlinx.coroutines.launch
 
+interface PokeUiState {
+    data class Success(val pokeList: String): PokeUiState
+    object Error: PokeUiState
+    object Loading: PokeUiState
+}
+
 class PokeViewModel: ViewModel() {
-    fun getPokeList() {
+    var pokeUiState: PokeUiState by mutableStateOf(PokeUiState.Loading)
+    private set
+
+    init {
+        getPokeList()
+    }
+
+    private fun getPokeList() {
         viewModelScope.launch {
             val list = PokeApi.retrofitService.getPokeInfo()
+            pokeUiState = PokeUiState.Success(list)
         }
     }
 }
