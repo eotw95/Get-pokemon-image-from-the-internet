e366cd2d772fd4d0b33f1702723e185c23df0343
Ryuji Koyama <ryujikoyama95@gmail.com>
2023-05-20 22:34:03 +0900

    Add HomeScreen

---
+++ app/src/main/java/com/example/pokelist/ui/theme/screens/HomeScreen.kt
@@ -0,0 +1,30 @@
+package com.example.pokelist.ui.theme.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HomeScreen(
+    pokeUiState: PokeUiState,
+    modifier: Modifier = Modifier
+) {
+    when(pokeUiState) {
+        is PokeUiState.Success -> ResultScreen(pokeUiState.pokeList, modifier)
+        is PokeUiState.Error -> ErrorScreen(modifier)
+        is PokeUiState.Loading -> LoadingScreen(modifier)
+    }
+}
+
+@Composable
+fun ResultScreen(pokeList: String, modifier: Modifier = Modifier) {
+    Text(text = pokeList)
+}
+
+@Composable
+fun ErrorScreen(modifier: Modifier = Modifier) {
+}
+
+@Composable
+fun LoadingScreen(modifier: Modifier = Modifier) {
+}
