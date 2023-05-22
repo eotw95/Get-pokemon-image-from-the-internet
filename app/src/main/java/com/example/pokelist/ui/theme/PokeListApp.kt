b8ae5972fe1be569597f268fd18d8848a6ab27ec
Ryuji Koyama <ryujikoyama95@gmail.com>
2023-05-23 00:46:16 +0900

    Fix PokeListApp

--- app/src/main/java/com/example/pokelist/ui/theme/PokeListApp.kt
+++ app/src/main/java/com/example/pokelist/ui/theme/PokeListApp.kt
@@ -2,11 +2,15 @@ package com.example.pokelist.ui.theme
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pokelist.ui.theme.screens.HomeScreen
 import com.example.pokelist.ui.theme.screens.PokeViewModel
 
 @Composable
 fun PokeListApp(modifier: Modifier = Modifier) {
-    val pokeViewModel = PokeViewModel()
+    // Composable内でviewModelをインスタンス化する際は
+    // lifecycle-viewmodel-composeライブラリのviewModel()を使用。シングルトン。
+    // 普通にViewModel()でインスタンス化すると再Composeが連発し大量のインスタンスが生成されてしまう。
+    val pokeViewModel: PokeViewModel = viewModel()
     HomeScreen(pokeUiState = pokeViewModel.pokeUiState)
 }
