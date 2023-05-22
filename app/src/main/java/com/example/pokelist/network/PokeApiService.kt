550d30a70c365d4257266e27aba05daedc0295d4
Ryuji Koyama <ryujikoyama95@gmail.com>
2023-05-22 23:09:54 +0900

    Fix code

--- app/src/main/java/com/example/pokelist/network/PokeApiService.kt
+++ app/src/main/java/com/example/pokelist/network/PokeApiService.kt
@@ -1,10 +1,11 @@
 package com.example.pokelist.network
 
-import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
-import kotlinx.serialization.json.Json
+//import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+//import kotlinx.serialization.json.Json
 import okhttp3.MediaType
 import retrofit2.Response
 import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
 
@@ -12,13 +13,13 @@ private const val BASE_URL = "https://pokeapi.co/api/v2/"
 
 private val retrofit = Retrofit
     .Builder()
-    .addConverterFactory(Json.asConverterFactory(MediaType.get("application/json")))
+    .addConverterFactory(ScalarsConverterFactory.create())
     .baseUrl(BASE_URL)
     .build()
 
 interface PokeApiService {
-    @GET("pokedex/{name}")
-    fun getPokeDex(@Path("name") name: String): Response<PokeDex>
+    @GET("berry/{id}/")
+    fun getPokeDex(@Path("id") id: String): String //Response<PokeDex>
 }
 
 object PokeApi {
