ec99c3c75fb432a65dfefbc7a0e4dcae12b0d473
Ryuji Koyama <ryujikoyama95@gmail.com>
2023-05-25 23:54:40 +0900

    Add data class pokemon

--- app/src/main/java/com/example/pokelist/network/Pokemon.kt
+++ app/src/main/java/com/example/pokelist/network/Pokemon.kt
@@ -1,9 +1,26 @@
 package com.example.pokelist.network
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class PokeDex(
-    val id: Int = 0,
-    val name: String = ""
+data class Pokemon(
+    val abilities: List<PokemonAbility>?,
+    @SerialName("base_experience") val baseExperience: Int?,
+    val forms: List<PokemonForm>?,
+    @SerialName("game_indices") val gameIndices: List<VersionGameIndex>?,
+    val height: Int?,
+    @SerialName("held_items") val heldItems: List<PokemonHeldItem>?,
+    val id: Int?,
+    @SerialName("is_default") val isDefault: Boolean?,
+    @SerialName("location_area_encounters") val locationAreaEncounters: String?,
+    val moves: List<PokemonMove>?,
+    val name: String,
+    val order: Int?,
+    @SerialName("past_types") val pastTypes: List<PastType>?,
+    val species: Species?,
+    val sprites: PokemonSprites?,
+    val stats: List<PokemonStat>?,
+    val types: List<PokemonType>?,
+    val weight: Int?,
 )
