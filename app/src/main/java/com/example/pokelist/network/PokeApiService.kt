9eaa3dcec074616b908ceff4efeaecd344bdd56b
Ryuji Koyama <ryujikoyama95@gmail.com>
2023-05-26 23:34:28 +0900

    Change ConverterFactory

--- app/src/main/java/com/example/pokelist/network/PokeApiService.kt
+++ app/src/main/java/com/example/pokelist/network/PokeApiService.kt
@@ -1,11 +1,10 @@
 package com.example.pokelist.network
 
-//import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
-//import kotlinx.serialization.json.Json
-import okhttp3.MediaType
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Response
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
 
@@ -13,15 +12,14 @@ private const val BASE_URL = "https://pokeapi.co/api/v2/"
 
 private val retrofit = Retrofit
     .Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
     .baseUrl(BASE_URL)
     .build()
 
 // Todo: data class定義してJson文字列をパースする
 interface PokeApiService {
-    @GET("berry/{id}/")
-    // @Pathで引数をエンドポイントに指定する
-    suspend fun getPokeData(@Path("id") id: String): String //Response<PokeDex>
+    @GET("pokemon/{name}/")
+    suspend fun getPokeData(@Path("name") name: String): Response<Pokemon>
 }
 
 object PokeApi {
